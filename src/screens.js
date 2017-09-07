60592ff46378a7b73c0d4266a77614337517f617
Kai Chang <Kai.Chang@news.com.au>
2017-09-07 18:39:57 +1000

    remove unused files

--- src/screens.js
+++ src/screens.js
@@ -1,12 +1,10 @@
 import { Navigation } from 'react-native-navigation';
 
-import Welcome from './containers/Welcome';
 import Categories from './containers/Categories';
 import SermonList from './containers/SermonList';
 import Sermon from './containers/Sermon';
 
 export function registerScreens(store, Provider) {
-  Navigation.registerComponent('efcSermon2.Welcome', () => Welcome, store, Provider);
   Navigation.registerComponent('efcSermon2.Categories',  () => Categories, store, Provider);
   Navigation.registerComponent('efcSermon2.SermonList', () => SermonList, store, Provider);
   Navigation.registerComponent('efcSermon2.Sermon',  () => Sermon, store, Provider);
